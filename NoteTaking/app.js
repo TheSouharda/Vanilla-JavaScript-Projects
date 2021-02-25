568876f535369e9d2fdfd0ab4ac05e0cfe97c8a4
Souharda Biswas <mail2souharda2015@gmail.com>
2021-02-25 17:03:07 +0530

    100doc_Od16

--- NoteTaking/app.js
+++ NoteTaking/app.js
@@ -3,19 +3,28 @@ const textInput = document.getElementById("textInput");
 const noteInput = document.getElementById("noteInput");
 const myForm = document.getElementById("myform");
 
-let myarr = [];
-let myobj = {};
+let note = {};
 
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const heading = headingInput.value.trim();
   const text = textInput.value.trim();
-  console.log(heading);
-  console.log(text);
 
-  myobj.heading = heading;
-  myobj.text = text;
+  let existingNotes = localStorage.getItem("allNotes");
+  if (existingNotes === null) {
+    existingNotes = [];
+  }
 
-  myarr.push(myobj);
-  console.log(myobj);
+  if (heading && text != null) {
+    note = {
+      heading: heading,
+      text: text,
+    };
+    existingNotes.push(note);
+    console.log(existingNotes);
+
+    localStorage.setItem("allNotes", JSON.stringify(existingNotes));
+  } else {
+    alert("Enter all the fields");
+  }
 });
