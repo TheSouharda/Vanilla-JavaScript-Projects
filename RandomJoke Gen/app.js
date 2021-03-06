ae22aac64e14e5016f34d846c73ea049536dfeb8
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-06 22:42:00 +0530

    100doc_Od24

--- RandomJoke Gen/app.js
+++ RandomJoke Gen/app.js
@@ -6,4 +6,23 @@ gen.addEventListener("click", function () {
   if (info.classList.contains("info")) {
     info.classList.remove("hide");
   }
+
+  getJoke();
 });
+
+const getJoke = async function () {
+  const res = await fetch(
+    `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,sexist,explicit&type=twopart`
+  );
+
+  const jokeInfo = await res.json();
+  console.log(jokeInfo);
+  console.log(jokeInfo.setup);
+  console.log(jokeInfo.delivery);
+
+  const html = ` <h3>${jokeInfo.setup}</h3>
+          <p>
+            ${jokeInfo.delivery}
+            </p>`;
+  info.innerHTML = html;
+};
