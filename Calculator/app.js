deacb729e57e70dc8b864558e6c888fe9e84e472
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-15 22:20:08 +0530

    100DOC

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,2 +1,16 @@
-const displayVal = document.querySelector(".calculator_display");
+const display = document.querySelector(".calculator_display");
 const keys = document.querySelector(".calculator_keys");
+
+console.log(typeof display.textContent);
+
+keys.addEventListener("click", (e) => {
+  const keyValue = e.target.textContent;
+  let displayValue = display.textContent;
+
+  if (displayValue === "0") {
+    console.log(keyValue);
+    display.textContent = keyValue;
+  } else {
+    display.textContent = displayValue + keyValue;
+  }
+});
