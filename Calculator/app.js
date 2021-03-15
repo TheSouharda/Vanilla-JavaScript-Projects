e0ac0d8252dd271feddface6011ee9791a6516f7
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-16 00:07:42 +0530

    100DOC

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,16 +1,24 @@
-const display = document.querySelector(".calculator_display");
-const keys = document.querySelector(".calculator_keys");
+const calculator = document.querySelector(".container");
+const display = calculator.querySelector(".calculator_display");
+const keys = calculator.querySelector(".calculator_keys");
 
 console.log(typeof display.textContent);
 
 keys.addEventListener("click", (e) => {
+  const key = e.target;
   const keyValue = e.target.textContent;
   let displayValue = display.textContent;
 
-  if (displayValue === "0") {
-    console.log(keyValue);
-    display.textContent = keyValue;
-  } else {
-    display.textContent = displayValue + keyValue;
+  if (key.classList.contains("number")) {
+    if (displayValue === "0") {
+      console.log(keyValue);
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+
+  if (key.dataset.type === "operator") {
+    console.log(keys);
   }
 });
