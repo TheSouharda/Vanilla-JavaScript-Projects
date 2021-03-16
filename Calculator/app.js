3d53bed6a1a2fa46624116b1b3a826cc45e20072
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-16 15:01:47 +0530

    100DOC

--- Calculator/app.js
+++ Calculator/app.js
@@ -8,17 +8,31 @@ keys.addEventListener("click", (e) => {
   const key = e.target;
   const keyValue = e.target.textContent;
   let displayValue = display.textContent;
+  const type = key.dataset.type;
+  const previousKeyType = calculator.dataset.previousKeyType;
 
-  if (key.classList.contains("number")) {
+  //? Is this an number key
+
+  if (type === "number") {
     if (displayValue === "0") {
       console.log(keyValue);
+      display.textContent = keyValue;
+    } else if (previousKeyType === "operator") {
+      //! This makes sure if the last thing pressed was an operator then the subsequent pressed number will be replaced in the display.
+
       display.textContent = keyValue;
     } else {
       display.textContent = displayValue + keyValue;
     }
+    calculator.dataset.previousKeyType = "number ";
   }
+  //? Is this an operator key
 
-  if (key.dataset.type === "operator") {
+  if (type === "operator") {
     console.log(keys);
+
+    //! We make add this dataset to make sure that the last thing pressed was an operator
+
+    calculator.dataset.previousKeyType = type;
   }
 });
